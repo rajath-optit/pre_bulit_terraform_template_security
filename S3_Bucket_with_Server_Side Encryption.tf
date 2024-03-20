84a4966e46f6c615bcc3e5312f57448526bd1b71
rajath-optit <128474801+rajath-optit@users.noreply.github.com>
2024-03-20 14:24:14 +0530

    Create S3_Bucket_with_Server_Side Encryption.tf

---
+++ S3_Bucket_with_Server_Side Encryption.tf
@@ -0,0 +1,13 @@
+# s3_bucket.tf
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-bucket"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
